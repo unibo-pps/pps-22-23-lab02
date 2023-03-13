b6e3de5eda6a909e6e63c3737cf1b9f8349b17a8
Gianluca Aguzzi <gianluca.aguzzi@studio.unibo.it>
2023-03-13 11:17:24 +0100

    feat: move project to SBT

---
+++ build.sbt
@@ -0,0 +1,3 @@
+scalaVersion := "3.2.2"
+
+libraryDependencies += "com.github.sbt" % "junit-interface" % "0.13.2" % Test
--- src/test/scala/u02/BTreesTest.scala
+++ src/test/scala/u02/BTreesTest.scala
@@ -1,21 +1,25 @@
 package u02
 
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.*
 import BTrees.Tree.*
+import junit.framework.TestCase.assertTrue
+import org.junit.Assert.{assertEquals, assertFalse}
+import org.junit.Test
 
 class BTreesTest:
 
   val tree = Branch(Branch(Leaf(1), Leaf(2)), Leaf(1))
 
-  @Test def testCount() =
+  @Test
+  def testCount() =
     assertEquals(2, count(tree, 1))
     assertEquals(1, count(tree, 2))
     assertEquals(0, count(tree, 3))
 
-  @Test def testSize() =
+  @Test
+  def testSize() =
     assertEquals(3, size(tree))
 
-  @Test def testFind() =
+  @Test
+  def testFind() =
     assertTrue(find(tree, 2))
     assertFalse(find(tree, 4))
